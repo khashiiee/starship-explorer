a1e5b3c8d7dcc8ba8e72336b608e0b9bdf64044a
Kavindi Peiris <kavindi.peiris@pearson.com>
2022-04-27 16:54:07 +0530

    feat: Add base components.

---
+++ components/Button/index.js
@@ -0,0 +1,13 @@
+function Button({ children, ...props }) {
+  return (
+    <button
+      className="h-10 px-6 font-semibold rounded-md border border-slate-200 text-slate-900"
+      type="button"
+      {...props}
+    >
+      {children}
+    </button>
+  );
+}
+
+export default Button;
---
+++ components/Card/index.js
@@ -0,0 +1,5 @@
+function Card({ children }) {
+  return <div className="bg-white shadow rounded">{children}</div>;
+}
+
+export default Card;
