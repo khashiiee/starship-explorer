b48146b709eaccb60aaf7d16089a14aedd5a3c06
Kavindi Peiris <kavindi.peiris@pearson.com>
2022-04-28 23:37:09 +0530

    feat: add movie details to movie page

--- pages/[movieId].js
+++ pages/[movieId].js
@@ -1,11 +1,25 @@
 import { useRouter } from "next/router";
+import useSWR from "swr";
 
 function MoviePage(props) {
   const { query } = useRouter();
 
+  const { data, error } = useSWR(
+    "https://swapi.dev/api/films/" + query.movieId
+  );
+
+  if (!data) return null;
+
   return (
     <div>
-      <h1>Movie: {query.movieId}</h1>
+      <h1 className="text-2xl	my-8">{data.title}</h1>
+      <div>Episode ID: {data.episode_id}</div>
+      <div>Opening Crawl: {data.opening_crawl}</div>
+      <div>Director: {data.director}</div>
+      <div>Producer: {data.producer}</div>
+      <div>
+        Release Date: {data.release_date.split("-").reverse().join("/")}
+      </div>
     </div>
   );
 }
